04ffa3e43569232b90d9dd12b651fb789eca41a5
esimm98 <esimm98@gmail.com>
2015-12-13 22:08:08 -0700

    Slight refactor before merge

--- shrine.rb
+++ shrine.rb
@@ -3,13 +3,9 @@ class Shrine < Enemy
 	def initialize(img, side)
 		super
 		if side == 0
-			@angle = 90
-			@rot_point = 1.0
-			@x = 50
+			@angle = 90; @rot_point = 1.0; @x = 50
 		else
-			@angle = -90
-			@rot_point = 0.0
-			@x = 400 - @height
+			@angle = -90; @rot_point = 0.0; @x = 400 - @height
 		end
 	end
 
